e2a810888b874e7912452dd7d30089bda330c751
quang <nguyenthequang080203@gmail.com>
2024-07-01 10:54:10 +0700

    add function write data to kafka topics

--- jobs/main.py
+++ jobs/main.py
@@ -5,6 +5,7 @@ import uuid
 from confluent_kafka import SerializingProducer
 import simplejson as json
 from datetime import datetime, timedelta
+import time
 import random
 
 LONDON_COORDINATES = { 'latitude': 51.5074, 'longitude': -0.1278 }
@@ -106,6 +107,25 @@ def generate_vehicle_data(deviceId):
         'fuelType': 'Gas'
     }
 
+def json_serializer(obj):
+    if isinstance(obj, uuid.UUID):
+        return str(obj)
+    raise TypeError(f'Object of Type {obj.__class__.__name__} is not JSON serializable')
+
+def delivery_report(err, msg):
+    if err is not None:
+        print(f'Message delivery failed: {err}')
+    else:
+        print(f'Message delivered to {msg.topic()} [{msg.partition()}]')
+
+def produce_data_to_kafka(producer, topic, data):
+    producer.produce(
+        topic,
+        key=str(data['id']),
+        value=json.dumps(data, default=json_serializer).encode('utf-8'),
+        on_delivery=delivery_report
+    ) 
+
 def simulate_journey(producer, deviceId): # Continuously generates vehicle data using generate_vehicle_data() and sends it to Kafka
     while True:
         vehicle_data = generate_vehicle_data(deviceId)
@@ -113,7 +133,23 @@ def simulate_journey(producer, deviceId): # Continuously generates vehicle data 
         traffic_camera_data = generate_traffic_camera_data(deviceId, vehicle_data['timestamp'], vehicle_data['location'], 'Sony-Cam1')
         weather_data = generate_weather_data(deviceId, vehicle_data['timestamp'], vehicle_data['location'])
         emergency_incident_data = generate_emergency_incident_data(deviceId, vehicle_data['timestamp'], vehicle_data['location'])
-        break
+
+        if (vehicle_data['location'][0] >= BIRMINGHAM_COORDINATES['latitude'] and vehicle_data['location'][1] <= BIRMINGHAM_COORDINATES['longitude']):
+            print('Vehicle has reached Birmingham. Simulation ending...')
+            break
+
+        # write into Kafka
+        produce_data_to_kafka(producer, VEHICLE_TOPIC, vehicle_data)
+        produce_data_to_kafka(producer, GPS_TOPIC, gps_data)
+        produce_data_to_kafka(producer, TRAFFIC_TOPIC, traffic_camera_data)
+        produce_data_to_kafka(producer, WEATHER_TOPIC, weather_data)
+        produce_data_to_kafka(producer, EMERGENCY_TOPIC, emergency_incident_data)
+
+        # Flush the producer periodically: Ensuring that all messages produced to Kafka are actually sent and acknowledged by the Kafka brokers
+        producer.flush()
+
+        # Simulate a delay
+        time.sleep(5)
 
 if __name__ == '__main__':
     producer_config = {
@@ -128,6 +164,6 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         print("Simulation ended by the user")
     except Exception as e:
-        print(f'Unexpected Error occurred')
+        print(f'Unexpected error occurred: {e}')
         
 
